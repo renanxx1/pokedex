d70fa1c2d9216ee2e9914519ccb99e224d586258
Renan <renan980@gmail.com>
2020-11-25 11:05:21 -0300

    pokedex

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke, index) in pokemons" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" /> 
+      </div>
+  </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("LISTA DE POKEMONS");
+        this.pokemons = res.data.results;
+        console.log(this.pokemons);
+      });
+  },
   components: {
-    HelloWorld
-  }
-}
+    Pokemon,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="container">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="pokemon.front" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left"></div>
+          <div class="media-content">
+            <p class="title is-4">{{ num }} - {{ name | upper }}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+
+        <div class="content"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created: function () {
+    axios.get(this.url).then((res) => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+    });
+  },
+  data() {
+    return {
+      pokemon: {
+        type: "",
+        front: "",
+        back: "",
+      },
+    };
+  },
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  filters: {
+    upper: function (value) {
+      var newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
+    },
+  },
+};
+</script>
+
+<style>
+#container{
+    margin-top: 2%;
+}
+
+</style>
