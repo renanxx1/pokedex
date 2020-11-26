11a368cc73efa10bf95cb989008b9c4b8a0d34ef
Renan <renan980@gmail.com>
2020-11-26 08:44:41 -0300

    pokedex com filtro de busca

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
-    <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke, index) in pokemons" :key="index">
-        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" /> 
-      </div>
-  </div>
+<div id="app">
+    <img src="./assets/logo.png" alt="" />
 
-  </div>
+    <div class="column is-half is-offset-one-quarter">
+        <hr />
+
+        <h4 class="is-size-4">POKEDEX</h4>
+        <input type="text" placeholder="Buscar Pokemon" v-model="busca" class="input is-rounded" />
+        <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">
+            Buscar
+        </button>
+        <div v-for="(poke, index) in filteredPokemons" :key="poke.name">
+            <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -14,34 +22,49 @@ import axios from "axios";
 import Pokemon from "./components/Pokemon";
 
 export default {
-  name: "App",
-  data() {
-    return {
-      pokemons: [],
-    };
-  },
-  created: function () {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-      .then((res) => {
-        console.log("LISTA DE POKEMONS");
-        this.pokemons = res.data.results;
-        console.log(this.pokemons);
-      });
-  },
-  components: {
-    Pokemon,
-  },
+    name: "App",
+    data() {
+        return {
+            pokemons: [],
+            busca: "",
+            filteredPokemons: [],
+        };
+    },
+    created: function () {
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+            .then((res) => {
+                this.pokemons = res.data.results;
+                this.filteredPokemons = res.data.results;
+            });
+    },
+    components: {
+        Pokemon,
+    },
+    methods: {
+        buscar: function () {
+            if (this.busca == "" || this.busca == " ") {
+                this.filteredPokemons = this.pokemons;
+            } else {
+                this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+            }
+
+        }
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+#buscaBtn {
+    margin-top: 1%;
+    margin-bottom: 2%;
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,62 +1,80 @@
 <template>
-  <div id="container">
-    <div class="card">
-      <div class="card-image">
-        <figure>
-          <img :src="pokemon.front" alt="Placeholder image" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="media">
-          <div class="media-left"></div>
-          <div class="media-content">
-            <p class="title is-4">{{ num }} - {{ name | upper }}</p>
-            <p class="subtitle is-6">{{ pokemon.type }}</p>
-          </div>
-        </div>
+<div id="container">
 
-        <div class="content"></div>
-      </div>
+    <div class="card">
+        <div class="card-image">
+            <figure>
+                <img :src="currentImage" alt="Placeholder image" />
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+                <div class="media-left"></div>
+                <div class="media-content">
+                    <p class="title is-4">{{ num }} - {{ name | upper }}</p>
+                    <p class="subtitle is-6">{{ pokemon.type }}</p>
+                </div>
+            </div>
+
+            <div class="content">
+                <button class="button is-medium is-fullwidth" @click="mudarSprite">
+                    Mudar Sprite
+                </button>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  created: function () {
-    axios.get(this.url).then((res) => {
-      this.pokemon.type = res.data.types[0].type.name;
-      this.pokemon.front = res.data.sprites.front_default;
-      this.pokemon.back = res.data.sprites.back_default;
-    });
-  },
-  data() {
-    return {
-      pokemon: {
-        type: "",
-        front: "",
-        back: "",
-      },
-    };
-  },
-  props: {
-    num: Number,
-    name: String,
-    url: String,
-  },
-  filters: {
-    upper: function (value) {
-      var newName = value[0].toUpperCase() + value.slice(1);
-      return newName;
+    created: function () {
+        axios.get(this.url).then((res) => {
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            this.currentImage = this.pokemon.front;
+        });
     },
-  },
+    data() {
+        return {
+            pokemon: {
+                type: "",
+                front: "",
+                back: "",
+            },
+            isFront: true,
+            currentImage: "",
+        };
+    },
+    props: {
+        num: Number,
+        name: String,
+        url: String,
+    },
+    filters: {
+        upper: function (value) {
+            var newName = value[0].toUpperCase() + value.slice(1);
+            return newName;
+        },
+    },
+    methods: {
+        mudarSprite: function () {
+            if (this.isFront) {
+                this.isFront = false;
+                this.currentImage = this.pokemon.back;
+            } else {
+                this.isFront = true;
+                this.currentImage = this.pokemon.front;
+            }
+        },
+    },
 };
 </script>
 
 <style>
-#container{
+#container {
     margin-top: 2%;
 }
-
 </style>
